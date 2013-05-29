770b91efa2485a043e3edc8aa9391bb04e35308e
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2013-05-29 17:44:58 +0200

    Release 0.2.0

--- tzf/pyramid_yml/__init__.py
+++ tzf/pyramid_yml/__init__.py
@@ -7,7 +7,7 @@ from pyramid.asset import resolve_asset_spec
 from pyramid.path import package_path
 from pymlconf import ConfigManager
 
-__version__ = '0.2.0dev'
+__version__ = '0.2.0'
 
 
 logger = logging.getLogger(__name__)
