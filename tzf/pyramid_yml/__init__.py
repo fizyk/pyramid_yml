4907ef3bd8f4256f017c4e10828bdd7a82911511
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2013-12-15 22:51:58 +0100

    "Release 0.3.0"

--- tzf/pyramid_yml/__init__.py
+++ tzf/pyramid_yml/__init__.py
@@ -12,7 +12,7 @@ from pyramid.asset import resolve_asset_spec
 from pyramid.path import package_path
 from pymlconf import ConfigManager
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 logger = logging.getLogger(__name__)
